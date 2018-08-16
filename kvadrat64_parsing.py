046660921d299cb40eed0812431dda4e475104f0
let-robots-reign <zotov.alexei.1@yandex.ru>
2018-08-16 17:18:59 +0400

    commercials parsing

--- kvadrat64_parsing.py
+++ kvadrat64_parsing.py
@@ -57,6 +57,20 @@ def get_price(soup):
     return price
 
 
+def get_commercial_price(soup):
+    price = "Не указано"
+    try:
+        aggregated = [x.find_all("span", class_="d") for x in soup.find_all("td", class_="tddec2")]  # список из всех ссылок из tddec2
+        flat_aggregated = [item for sublist in aggregated for item in sublist]  # из двумерного списка делаем одномерный
+        price_params = [x.text.strip() for x in flat_aggregated]
+        for param in price_params:
+            if "за м²" in param:
+                price = "м2".join(param.split("м²"))
+    except Exception as e:
+        print(e)
+    return price
+
+
 def get_selling_type(soup):
     try:
         # если продажа, ищем тип продажи
@@ -199,6 +213,24 @@ def get_cottage_params(soup):
     return total_area, material
 
 
+def get_commercial_params(soup):
+    object_type = "Не указано"
+    try:
+        ###
+        # из-за кривой структуры сайта, формируем все сами в удобный формат
+        params_raw = str(soup.find("td", class_="tddec")).split("<br/>")
+        params = BeautifulSoup(params_raw[0], "lxml").find("td", class_="tddec").text.strip().split("\xa0")
+        for param in params_raw[1:]:
+            params.append(BeautifulSoup(param, "lxml").text.strip())
+        ###
+        for param in params:
+            if "Объект" in param:
+                object_type = param.split(":")[1].strip()
+    except Exception as e:
+        print(e, "cottage params")
+    return object_type
+
+
 def write_csv(data, category):
     if category == "apartments":
         with open("kvadrat_apartments.csv", "a") as csv_file:
@@ -218,46 +250,68 @@ def get_apartment_data(html, url):
     soup = BeautifulSoup(html, "lxml")
 
     title = get_title(soup)
-    address = "".join(title.split(",")[1:]).strip()
-    address = address[:address.rfind(" на карте")]
-    if "сдам" in address.lower():
-        address = " ".join(address.split()[1:])
-    rooms_number = title.split(",")[0]
-    block_type, total_area, total_floors, material = get_apartment_params(soup)
-    price = get_price(soup)
-    selling_type, rent_info = get_selling_type(soup)  # чистая продажа/ипотека/без посредников; если аренда, срок аренды
-    if not selling_type:
-        selling_type = "Не продажа"
-    images = get_photos(soup)
-    description = get_description(soup)
-    phone = get_seller_phone(url, soup)
-    date = get_date(soup)
+    if "сниму" not in title.lower():
+        address = "".join(title.split(",")[1:]).strip()
+        address = address[:address.rfind(" на карте")]
+        if "сдам" in address.lower():
+            address = " ".join(address.split()[1:])
+        rooms_number = title.split(",")[0]
+        block_type, total_area, total_floors, material = get_apartment_params(soup)
+        price = get_price(soup)
+        selling_type, rent_info = get_selling_type(soup)  # чистая продажа/ипотека/без посредников; если аренда, срок аренды
+        if not selling_type:
+            selling_type = "Не продажа"
+        images = get_photos(soup)
+        description = get_description(soup)
+        phone = get_seller_phone(url, soup)
+        date = get_date(soup)
 
-    return [address, rent_info, block_type, rooms_number, price, total_area, total_floors, material, selling_type,
-            images, description, phone, date]
+        return [address, rent_info, block_type, rooms_number, price, total_area, total_floors, material, selling_type,
+                images, description, phone, date]
+    return None
 
 
 def get_cottage_data(html, url):
     soup = BeautifulSoup(html, "lxml")
 
     title = get_title(soup)
-    address = "".join(title.split(",")[1:]).strip()
-    address = address[:address.rfind(" на карте")]
-    cottage_type = title.split(",")[0]
-    if "сдам" in cottage_type.lower():
-        cottage_type = " ".join(cottage_type.split()[1:])
-    price = get_price(soup)
-    total_area, material = get_cottage_params(soup)
-    selling_type, rent_info = get_selling_type(soup)  # чистая продажа/ипотека/без посредников; если аренда, срок аренды
-    if not selling_type:
-        selling_type = "Не продажа"
-    images = get_photos(soup)
-    description = get_description(soup)
-    phone = get_seller_phone(url, soup)
-    date = get_date(soup)
+    if "сниму" not in title.lower():
+        address = "".join(title.split(",")[1:]).strip()
+        address = address[:address.rfind(" на карте")]
+        cottage_type = title.split(",")[0]
+        if "сдам" in cottage_type.lower():
+            cottage_type = " ".join(cottage_type.split()[1:])
+        price = get_price(soup)
+        total_area, material = get_cottage_params(soup)
+        selling_type, rent_info = get_selling_type(soup)  # чистая продажа/ипотека/без посредников; если аренда, срок аренды
+        if not selling_type:
+            selling_type = "Не продажа"
+        images = get_photos(soup)
+        description = get_description(soup)
+        phone = get_seller_phone(url, soup)
+        date = get_date(soup)
 
-    return [address, rent_info, cottage_type, price, total_area, selling_type, material,
-            images, description, phone, date]
+        return [address, rent_info, cottage_type, price, total_area, selling_type, material,
+                images, description, phone, date]
+    return None
+
+
+def get_commercial_data(html, url):
+    soup = BeautifulSoup(html, "lxml")
+
+    title = get_title(soup)
+    if "сниму" not in title.lower():
+        address = "".join(title.split(",")[1:]).strip()
+        address = address[:address.rfind(" на карте")]
+        object_type = get_commercial_params(soup)
+        price = get_commercial_price(soup)
+        images = get_photos(soup)
+        description = get_description(soup)
+        phone = get_seller_phone(url, soup)
+        date = get_date(soup)
+
+        return [address, object_type, price, images, description, phone, date]
+    return None
 
 
 def crawl_page(html, category, sell_type):
@@ -269,16 +323,17 @@ def crawl_page(html, category, sell_type):
             data = []
             if category == "apartments":
                 data = get_apartment_data(get_html(url), url)
-            # elif category == "commercials":
-            #     data = get_commercial_data(get_html(url))
+            elif category == "commercials":
+                data = get_commercial_data(get_html(url), url)
             elif category == "cottages":
                 data = get_cottage_data(get_html(url), url)
 
-            if sell_type == "Аренда":
-                data.insert(1, sell_type + "; " + data[1])  # прибавляем срок аренды
-            else:
-                data.insert(1, sell_type)
-            data.pop(2)  # и удаляем срок аренды как отдельный элемент списка
+            if category == "apartments" or category == "cottages":
+                if sell_type == "Аренда":
+                    data.insert(1, sell_type + "; " + data[1])  # прибавляем срок аренды
+                else:
+                    data.insert(1, sell_type)
+                data.pop(2)  # и удаляем срок аренды как отдельный элемент списка
 
             # if data[-1] < datetime.datetime.today():
             #     # сраниваем форматы datetime, чтобы знать, когда закончить парсинг
@@ -319,16 +374,6 @@ def parse(category_url, category_name, sell_type):
 
 
 def main():
-    with open("kvadrat_apartments.csv", "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=";")
-        writer.writerow(["Адрес", "Тип сделки", "Тип дома", "Количество комнат", "Цена",
-                         "Общая площадь", "Количество этажей", "Материал стен", "Тип продажи"
-                         "Фотографии", "Описание", "Номер телефона", "Дата"])
-    with open("kvadrat_cottages.csv", "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=";")
-        writer.writerow(["Адрес", "Тип сделки", "Тип дома", "Цена", "Общая площадь", "Тип продажи",
-                         "Материал", "Фотографии", "Описание", "Дата", "Номер телефона"])
-
     url_apartments_sell = "http://kvadrat64.ru/sellflatbank-50-1.html"
     parse(url_apartments_sell, "apartments", "Продажа")
 
@@ -340,6 +385,12 @@ def main():
 
     url_cottages_rent = "https://kvadrat64.ru/giveflatbank-9-1.html"
     parse(url_cottages_rent, "cottages", "Аренда")
+
+    url_commercials_sell = "https://kvadrat64.ru/sellcombank-1000-1.html"
+    parse(url_commercials_sell, "commercials", "Продажа")
+
+    url_commercials_rent = "https://kvadrat64.ru/givecombank-1000-1.html"
+    parse(url_commercials_rent, "commercials", "Аренда")
 
 
 if __name__ == "__main__":
