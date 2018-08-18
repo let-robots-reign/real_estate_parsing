0154b8d33d5be1c0b4823974d9474b6c0382ed4f
let-robots-reign <zotov.alexei.1@yandex.ru>
2018-08-18 20:38:08 +0400

    parse all categories (no anti-ban solution)

--- ya_realty_parsing.py
+++ ya_realty_parsing.py
@@ -10,7 +10,11 @@ import os
 
 
 def get_html(url):
-    req = requests.get(url, headers={"User-Agent": UserAgent().chrome})
+    req = requests.get(url, headers={"User-Agent": UserAgent().chrome, "Referer": url,
+                                     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                                     "Connection": "keep-alive", "Origin": "https://realty.yandex.ru",
+                                     "DNT": "1"})
+    print(req.headers)
     return req.text.encode(req.encoding)
 
 
@@ -109,21 +113,13 @@ def get_date(soup):
     return date
 
 
-def get_seller_phone(url):
+def get_seller_phone(soup):
     phone = "Не указано"
-    # телефон появляется динамически, используем selenium
     try:
-        driver = webdriver.Chrome(executable_path=chrome_driver)
-
-        driver.get(url)
-        button = driver.find_element_by_xpath('//button[@class="button button_theme_new-action stat phones__button '
-                                              'i-bem stat_ecommerce_yes stat_conversion_yes stat_criteo_yes '
-                                              'stat_goal_yes stat_js_inited button_js_inited"]')
-        button.click()
-        time.sleep(3)
-        phone = driver.find_element_by_xpath('//div[@class="helpful-info__contact-phones-string"').text
+        phone_div = soup.find("div", class_="offer-card__contacts-new-phones").find("div").get("data-bem")
+        print(phone_div)
     except Exception as e:
-        print(str(e) + " phone")
+        print(e)
     return phone
 
 
@@ -132,21 +128,78 @@ def get_apartment_params(soup):
     try:
         params = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-name")]
         values = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-value")]
-        pairs = dict(zip(params, values))
-        for param, value in pairs:
-            if "Количество комнат" in param:
-                rooms_number = value
-            elif "Год постройки" in param:
-                year = value
-            elif "Этаж" in param:
-                total_floors = value
-            elif "Общая площадь" in param:
-                total_area = value
-            elif "Тип здания" in param:
-                material = value
+        for i in range(len(params)):
+            if "Количество комнат" in params[i]:
+                rooms_number = values[i]
+            elif "Год постройки" in params[i]:
+                year = values[i]
+            elif "Этаж" in params[i]:
+                total_floors = values[i]
+            elif "Общая площадь" in params[i]:
+                total_area = values[i]
+            elif "Тип здания" in params[i]:
+                material = values[i]
+
+        if year == "Не указано":
+            new_block_params = [x.text.strip() for x in soup.find_all("div", class_="offer-card__site-subtitle-item")]
+            for param in new_block_params:
+                if "строится" in param:
+                    year = param
     except Exception as e:
-        print(str(e) + " params")
+        print(str(e) + " apartment params")
     return rooms_number, total_floors, total_area, material, year
+
+
+def get_cottage_params(soup):
+    total_area, land_area, comforts, year = ["Не указано"] * 4
+    try:
+        params = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-name")]
+        values = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-value")]
+        for i in range(len(params)):
+            if "Год постройки" in params[i]:
+                year = values[i]
+            elif "Общая площадь" in params[i]:
+                total_area = values[i]
+            elif "Площадь участка" in params[i]:
+                land_area = values[i]
+            elif any(x in params[i].lower() for x in ["отапливаемый", "отопление", "водопровод", "канализация",
+                                                      "электроснабжение", "свет", "газ", "вода", "интернет",
+                                                      "телефон", "душ"]):
+                if comforts == "Не указано":
+                    comforts = params[i].strip()
+                else:
+                    comforts += "; " + params[i].strip()
+
+        if year == "Не указано":
+            new_block_params = [x.text.strip() for x in soup.find_all("div", class_="offer-card__site-subtitle-item")]
+            for param in new_block_params:
+                if "строится" in param:
+                    year = param
+    except Exception as e:
+        print(str(e) + " cottage params")
+    return total_area, land_area, comforts, year
+
+
+def get_commercial_params(soup):
+    entrance, furniture, additions = ["Не указано"] * 3
+    try:
+        params = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-name")]
+        values = [x.text.strip() for x in soup.find_all("div", class_="offer-card__feature-value")]
+        for i in range(len(params)):
+            if "Мебель" in params[i]:
+                furniture = values[i]
+            elif "Вход" in params[i]:
+                entrance = values[i]
+            elif any(x in params[i].lower() for x in ["кондиционер", "интернет", "пожарная сигнализация",
+                                                      "вентиляция", "охраняемая парковка", "сигнализация", "лифт"])\
+                    and values[i].strip() == "да":
+                if additions == "Не указано":
+                    additions = params[i].strip()
+                else:
+                    additions += "; " + params[i].strip()
+    except Exception as e:
+        print(str(e) + " cottage params")
+    return entrance, furniture, additions
 
 
 def get_apartment_data(html, url):
@@ -161,10 +214,39 @@ def get_apartment_data(html, url):
     seller_type = get_seller_type(soup)
     images = get_photos(soup)
     description = get_description(soup)
-    phone = get_seller_phone(url)
+    phone = get_seller_phone(soup)
 
     return [address, block_type, rooms_number, price, total_area, total_floors, material,
             year, selling_type, images, description, seller_type, phone]
+
+
+def get_cottage_data(html):
+    soup = BeautifulSoup(html, "lxml")
+
+    title = get_title(soup)
+    address = get_address(soup)
+    cottage_type = title
+    price = get_price(soup)
+    total_area, land_area, comforts, year = get_cottage_params(soup)
+    selling_type = get_selling_type(soup)
+    images = get_photos(soup)
+    description = get_description(soup)
+    phone = get_seller_phone(soup)
+
+    return [address, cottage_type, price, total_area, land_area, comforts, year, selling_type,
+            images, description, phone]
+
+
+def get_commercial_data(html):
+    soup = BeautifulSoup(html, "lxml")
+
+    title = get_title(soup)
+    address = get_address(soup)
+    object_type = title.split(",")[0]
+    entrance, furniture, additions = get_commercial_params(soup)
+    phone = get_seller_phone(soup)
+
+    return [address, object_type, furniture, entrance, additions, phone]
 
 
 def crawl_page(html, category, sell_type):
@@ -176,6 +258,7 @@ def crawl_page(html, category, sell_type):
         return
 
     offers = soup.find("ol", class_="OffersSerp__list").find_all("li", class_="OffersSerp__list-item_type_offer")
+    k = 0
     for offer in offers:
         try:
             # TODO: проверить еще на дубликат
@@ -187,10 +270,10 @@ def crawl_page(html, category, sell_type):
             data = []
             if category == "apartments":
                 data = get_apartment_data(get_html(url), url)
-            # elif category == "commercials":
-            #     data = get_commercial_data(get_html(url))
-            # elif category == "cottages":
-            #     data = get_cottage_data(get_html(url))
+            elif category == "commercials":
+                data = get_commercial_data(get_html(url))
+            elif category == "cottages":
+                data = get_cottage_data(get_html(url))
 
             date = get_date(soup)
             data.append(date)
@@ -202,7 +285,11 @@ def crawl_page(html, category, sell_type):
             print(e)
             print("Ошибка в crawl_page")
 
-        time.sleep(random.uniform(8, 11))
+        k += 1
+        if k % 5 == 0:  # после каждого пятого запроса, делаем паузу побольше
+            time.sleep(120)
+        else:
+            time.sleep(random.uniform(15, 20))
 
 
 def parse(category_url, category_name, sell_type):
@@ -219,6 +306,21 @@ def parse(category_url, category_name, sell_type):
 def main():
     url_apartments_sell = "https://realty.yandex.ru/saratovskaya_oblast/kupit/kvartira/?sort=DATE_DESC&page=0"
     parse(url_apartments_sell, "apartments", "Продажа")
+
+    url_apartments_rent = "https://realty.yandex.ru/saratovskaya_oblast/snyat/kvartira/?sort=DATE_DESC&page=0"
+    parse(url_apartments_rent, "apartments", "Аренда")
+
+    url_cottages_sell = "https://realty.yandex.ru/saratovskaya_oblast/kupit/dom/?sort=DATE_DESC&page=0"
+    parse(url_cottages_sell, "cottages", "Продажа")
+
+    url_cottages_rent = "https://realty.yandex.ru/saratovskaya_oblast/snyat/dom/?sort=DATE_DESC&page=0"
+    parse(url_cottages_rent, "cottages", "Аренда")
+
+    url_commercials_sell = "https://realty.yandex.ru/saratovskaya_oblast/kupit/kommercheskaya-nedvizhimost/?sort=DATE_DESC&page=0"
+    parse(url_commercials_sell, "commercials", "Продажа")
+
+    url_commercials_rent = "https://realty.yandex.ru/saratovskaya_oblast/snyat/kommercheskaya-nedvizhimost/?sort=DATE_DESC&page=0"
+    parse(url_commercials_rent, "commercials", "Аренда")
 
 
 if __name__ == "__main__":
