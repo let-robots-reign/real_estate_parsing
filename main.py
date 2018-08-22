d9813aadf0ea38426340a319c15a2c7d609fe3d0
let-robots-reign <zotov.alexei.1@yandex.ru>
2018-08-22 20:25:18 +0400

    main function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import kvadrat64_parsing
 import ya_realty_parsing
 import cian_parsing
 import youla_parsing
-import threading
+from multiprocessing import Process
 import os
 
 if os.path.isfile("total_data.txt"):
@@ -13,30 +13,16 @@ if os.path.isfile("total_data.txt"):
 if os.path.isfile("logs.txt"):
     os.remove("logs.txt")
 
-avito = avito_parsing.main()
-irr = irr_parsing.main()
 kvadrat = kvadrat64_parsing.main()
+irr = irr_parsing.main()
+youla = youla_parsing.main()
 ya = ya_realty_parsing.main()
 cian = cian_parsing.main()
-youla = youla_parsing.main()
+avito = avito_parsing.main()
 
-t1 = threading.Thread(target=kvadrat)
-t2 = threading.Thread(target=irr)
-t3 = threading.Thread(target=ya)
-t4 = threading.Thread(target=youla)
-t5 = threading.Thread(target=cian)
-t6 = threading.Thread(target=avito)
-
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
-
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
-t6.join()
+t1 = Process(target=kvadrat).start()
+t2 = Process(target=irr).start()
+t3 = Process(target=youla).start()
+t4 = Process(target=ya).start()
+t5 = Process(target=cian).start()
+t6 = Process(target=avito).start()
